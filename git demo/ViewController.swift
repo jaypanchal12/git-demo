aae4977c99e7234553b7077dcb8970cc1a68f9e7
jay <jay@upskwt.com>
2018-02-01 09:15:19 +0300

    Initial Commit

---
+++ git demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git demo
+//
+//  Created by KWTMAC01 on 2/1/18.
+//  Copyright © 2018 KWTMAC01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
